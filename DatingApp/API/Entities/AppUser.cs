b65419890643874d4966e8c3e8e144bcc009f808
LeonVieira <LeonVieira456@gmail.com>
2024-01-05 09:10:44 +0200

    Test

---
+++ DatingApp/API/Entities/AppUser.cs
@@ -0,0 +1,10 @@
+﻿namespace API;
+
+public class AppUser
+{
+    public string Id { get; set; }
+    public string UserName { get; set; }
+}
+
+
+
---
+++ DatingApp/API/Program.cs
@@ -0,0 +1,29 @@
+using API;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+
+//Swagger Code
+builder.Services.AddControllers();
+// // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<DataContext>(opt =>
+{
+    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+var app = builder.Build();
+
+
+
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
